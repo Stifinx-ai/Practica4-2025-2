256c8694ff7c6b39e64f83896af4ec4c0195bf9e
Stifinx-ai <yarethavf@gmail.com>
2025-03-06 14:14:48 -0600

    Create Ejercicio2.c

---
+++ Ejercicio2.c
@@ -0,0 +1,21 @@
+//Avila Flores Ahmed
+//Diagrama de flujo
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 3;
+    int b = 2;
+    
+    if (a>b)
+    {
+        printf("a es mayor \n");
+    }
+    else
+    {
+        printf("b es mayor o igual a \n");
+    }
+    return 0;
+    
+}
